b70937c91be53ed61c7560e1f8e4b00f0225a3b3
vikash12345 <vikash@backgroundcheck.email>
2017-02-02 16:43:22 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -29,6 +29,7 @@
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
 $html_content = scraperwiki::scrape("http://www.whatstove.co.uk/advanced-stove-search/search-results/?criteria=2&jr_okforsmokelesszone=1&order=alpha&query=any/");
+$html = str_get_html($html_content);
 
-print_r($html_content);
+print_r($html);
   ?>
